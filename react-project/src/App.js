a45ce33d5f8f4bc09d4e7f9ef80a01150a18eed5
Varsha Singh <varsha.singh0497@gmail.com>
2021-11-11 17:13:21 +0530

    fix: restructuring project

---
+++ react-project/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css';
+import { Header } from "./components/Header";
+
+function App() {
+  return (
+    <div>
+      Hello World
+    </div>
+  );
+}
+
+export default App;
