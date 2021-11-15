86d9799444b1ac0d1ebf057a3386e583cf26a278
Varsha Singh <varsha.singh0497@gmail.com>
2021-11-15 20:49:36 +0530

    feat: handle signs according to negative or positive amount

---
+++ react-project/src/components/Transaction.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Transaction = ({ transaction }) => {
+    const sign = transaction.amount < 0 ? '-' : '+';
+
+    return (
+        <li className="minus">
+            {transaction.text} <span>{sign}₹{Math.abs(transaction.amount)}</span><button className="delete-btn">x</button>
+        </li>
+    )
+}
